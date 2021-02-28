fa5424118a3c69b847c4b6fd944f550b3138250b
Paulo <paulotcj@gmail.com>
2021-02-28 10:09:27 -0500

    first commit

---
+++ testmvc_vue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testmvc_vue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
