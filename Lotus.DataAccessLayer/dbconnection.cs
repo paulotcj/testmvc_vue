1b62f45124862308050d0f9e552b3862a3040b48
roman <roman@dealerpilothr.com>
2021-03-15 17:23:08 -0400

    init commit

---
+++ Lotus.DataAccessLayer/dbconnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lotus.DataAccessLayer
+{
+
+
+    public class dbconnection
+    {
+            //     using (var connection = new NpgsqlConnection("User ID=root;Password=myPassword;Host=localhost;Port=5432;Database=myDataBase;"))
+            //{
+	           // connection.Query<MyTable>("SELECT * FROM MyTable");
+            //}
+
+
+
+
+    }
+}
