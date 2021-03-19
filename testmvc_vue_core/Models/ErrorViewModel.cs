9402006a7ab96f147b39061cf01014455008b43e
Paulo <paulotcj@gmail.com>
2021-03-19 17:05:59 -0400

    jhgfkfghfjkfgyhjkghuj

---
+++ testmvc_vue_core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testmvc_vue_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ testmvc_vue_core/Models/TestA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testmvc_vue_core.Models
+{
+    public class TestA
+    {
+
+        public int TestAID { get; set; }
+
+        [Display(Name = "Document Code")]
+        [Required(ErrorMessage = "DocumentCode_required")]
+        [DataType(DataType.Text)]
+        [StringLength(15, MinimumLength = 0, ErrorMessage = "DocumentCode_wrong_length")]
+        public string DocumentCode { get; set; }
+    }
+}
